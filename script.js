6816711ef6e664446e8de434bdb1778d720e465b
Johanna-hub <jyherman@gmail.com>
2019-01-14 14:38:26 +0000

    Example one refactor

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var show = function() {
+function unlock() {
+    document.documentElement.requestFullscreen();
+    screen.orientation.unlock();
+    console.log("Screen unlocked to" + screen.orientation.type);
+}
+
+const { type, angle } = screen.orientation
+function show(event) {
    console.log("Orientation type is " + screen.orientation.type);
    console.log("Orientation angle is " + screen.orientation.angle);
+   console.log("Event Type is " + event.type)
 }
 
 screen.orientation.addEventListener("change", show);
