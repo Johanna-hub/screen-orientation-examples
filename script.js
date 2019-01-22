56954934698ba8d8f94b9907f9cebaec13e99c62
Johanna-hub <jyherman@gmail.com>
2019-01-22 11:13:42 +0000

    example 3

--- script.js
+++ script.js
@@ -1,76 +1,83 @@
-// async function goFullScreen() {
-//     if(document.fullscreenElement === null) {
-//         await document.documentElement.requestFullscreen();
-//     } 
-// }
+async function goFullScreen() {
+  if (document.fullscreenElement === null) {
+    await document.documentElement.requestFullscreen();
+  }
+}
 
-// async function unlock() {
-//     await goFullScreen();
-//     await screen.orientation.unlock();
-// }
+async function unlock() {
+  await goFullScreen();
+  await screen.orientation.unlock();
+}
 
-// async function lockPortrait(){
-//     await goFullScreen();
-//     await screen.orientation.lock('portrait');
-// }
+async function lockPortrait() {
+  await goFullScreen();
+  await screen.orientation.lock("portrait");
+}
 
-// async function lockLandscape(){
-//     await goFullScreen();
-//     await screen.orientation.lock('landscape');
-// }
+async function lockLandscape() {
+  await goFullScreen();
+  await screen.orientation.lock("landscape");
+}
 
-// const { type, angle } = screen.orientation
-// function show(event) {
-//    console.log("Orientation type is " + screen.orientation.type);
-//    console.log("Orientation angle is " + screen.orientation.angle);
-//    console.log("Event Type is " + event.type)
-// }
+function show(event) {
+  const { type, angle } = screen.orientation;
+  console.log(`Orientation type is ${type} + angle is ${angle}`);
+  console.log("Event Type is " + event.type);
+}
 
-// screen.orientation.addEventListener("change", show);
-// window.onload = show;
+screen.orientation.addEventListener("change", show);
+window.onload = show;
 
-// function ready(){
-//     console.log("Now full screen and ready to start, locked to " + screen.orientation.type);
-// }
+async function rotate() {
+  const rotate = document.getElementById("rotate");
+  await goFullScreen();
+  const newOrientation = screen.orientation.type.startsWith("portrait")
+    ? "landscape"
+    : "portrait";
+  console.log(`New Orientation is ${newOrientation}`);
+  await screen.orientation.lock(newOrientation);
+  rotate.textContent = `Rotate to ${newOrientation}`;
+}
 
-// async function start() {
-//     const promiseToFullScreen = new Promise(resolve =>
-//       document.addEventListener("fullscreenchange", resolve)
-//     );
-//     await document.documentElement.requestFullscreen();
-//     await promiseToFullScreen;
-//     await screen.orientation.lock('landscape');
-//     ready();
-//   }
+function ready() {
+  console.log(
+    "Now full screen and ready to start, locked to " + screen.orientation.type
+  );
+}
+
+async function start() {
+  const promiseToFullScreen = new Promise(resolve =>
+    document.addEventListener("fullscreenchange", resolve)
+  );
+  await document.documentElement.requestFullscreen();
+  await promiseToFullScreen;
+  await screen.orientation.lock("landscape");
+  ready();
+}
 
 function manualRotation() {
-    alert("Thank you for rotating!");
+  alert("Thank you for rotating!");
+}
+
+function final() {
+  alert("thanks for turning");
+}
+async function start2() {
+  try {
+    await screen.orientation.lock("landscape");
+    final();
+  } catch (err) {
+    console.error(err);
   }
-  
-  function orientationChangeHandler() {
-    if (window.matchMedia("(orientation: portrait)").matches) {
-        console.log(screen.orientation.type, "so type");
-        console.log(window.matchMedia)
-      return;
-    }
-    window.removeEventListener("orientationchange", orientationChangeHandler);
-    manualRotation();
-  }
-  
-  async function start() {
-    try {
-      await screen.orientation.lock("landscape");
-    } catch {
-      window.addEventListener("orientationchange", orientationChangeHandler);
-      alert("To start, please rotate your screen to landscape");
-    }
-  }
-  
-  function thanks() {
-    alert("Rotated");
-    window.removeEventListener("orientationchange", orientationChangeHandler);
-  }
-  
-  function rotate() {
-    window.addEventListener("orientationchange", thanks);
-  }
+  const matchLandscape = matchMedia("(orientation: landscape)");
+  if (matchLandscape.matches) return final(); 
+  addEventListener("orientationchange", function listener() {
+    matchLandscape.addListener(function mediaChange(e) {
+      if (!e.matches) return; // Nope, not landscape!
+      removeEventListener("orientationchange", listener);
+      matchLandscape.removeListener(mediaChange);
+      final();
+    });
+  });
+  alert("To start, please rotate your screen to landscape.");
+}
