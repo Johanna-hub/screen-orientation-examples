97f5fb2d07fabf68bb3ee94f24578ffe201dfc28
Johanna-hub <jyherman@gmail.com>
2019-01-23 14:16:33 +0000

    test

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ function goFullScreen() {
   return document.body.requestFullscreen();
 }
 
-async function unlock() {
-  await goFullScreen();
-  await screen.orientation.unlock();
-}
+// async function unlock() {
+//   await goFullScreen();
+//   await screen.orientation.unlock();
+// }
 
-async function lockPortrait() {
-  await goFullScreen();
-  await screen.orientation.lock("portrait");
-}
+// async function lockPortrait() {
+//   await goFullScreen();
+//   await screen.orientation.lock("portrait");
+// }
 
-async function lockLandscape() {
-  await goFullScreen();
-  await screen.orientation.lock("landscape");
-}
+// async function lockLandscape() {
+//   await goFullScreen();
+//   await screen.orientation.lock("landscape");
+// }
 
-function show(event) {
-  const { type, angle } = screen.orientation;
-  console.log(`Orientation type is ${type} + angle is ${angle}.`);
-  console.log("Event Type is " + event.type);
-}
+// function show(event) {
+//   const { type, angle } = screen.orientation;
+//   console.log(`Orientation type is ${type} + angle is ${angle}.`);
+//   console.log("Event Type is " + event.type);
+// }
 
 async function rotate() {
   const rotateButton = document.getElementById("rotateButton");
@@ -36,6 +36,7 @@ async function rotate() {
   console.log(`New Orientation is ${newOrientation}`);
   await screen.orientation.lock(newOrientation);
   rotateButton.textContent = `Rotate to ${newOrientation}`;
+  console.log(rotateButton.textContent);
 }
 
 screen.orientation.addEventListener("change", show);
@@ -47,39 +48,39 @@ function ready() {
   );
 }
 
-async function start() {
-  const promiseToFullScreen = new Promise(resolve =>
-    document.addEventListener("fullscreenchange", resolve)
-  );
-  await document.documentElement.requestFullscreen();
-  await promiseToFullScreen;
-  await screen.orientation.lock("landscape");
-  ready();
-}
+// async function start() {
+//   const promiseToFullScreen = new Promise(resolve =>
+//     document.addEventListener("fullscreenchange", resolve)
+//   );
+//   await document.documentElement.requestFullscreen();
+//   await promiseToFullScreen;
+//   await screen.orientation.lock("landscape");
+//   ready();
+// }
 
-function manualRotation() {
-  alert("Thank you for rotating!");
-}
+// function manualRotation() {
+//   alert("Thank you for rotating!");
+// }
 
-function final() {
-  alert("thanks for turning");
-}
-async function start2() {
-  try {
-    await screen.orientation.lock("landscape");
-    final();
-  } catch (err) {
-    console.error(err);
-  }
-  const matchLandscape = matchMedia("(orientation: landscape)");
-  if (matchLandscape.matches) return final();
-  addEventListener("orientationchange", function listener() {
-    matchLandscape.addListener(function mediaChange(e) {
-      if (!e.matches) return; // Nope, not landscape!
-      removeEventListener("orientationchange", listener);
-      matchLandscape.removeListener(mediaChange);
-      final();
-    });
-  });
-  alert("To start, please rotate your screen to landscape.");
-}
+// function final() {
+//   alert("thanks for turning");
+// }
+// async function start2() {
+//   try {
+//     await screen.orientation.lock("landscape");
+//     final();
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   const matchLandscape = matchMedia("(orientation: landscape)");
+//   if (matchLandscape.matches) return final();
+//   addEventListener("orientationchange", function listener() {
+//     matchLandscape.addListener(function mediaChange(e) {
+//       if (!e.matches) return; // Nope, not landscape!
+//       removeEventListener("orientationchange", listener);
+//       matchLandscape.removeListener(mediaChange);
+//       final();
+//     });
+//   });
+//   alert("To start, please rotate your screen to landscape.");
+// }
