94b4b120354039b3f1d85de9ea69b58103a5c10d
Johanna-hub <jyherman@gmail.com>
2019-01-24 09:46:39 +0000

    example 1 spec

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-function goFullScreen() {
-  if (document.fullscreenElement) return;
-  return document.documentElement.requestFullscreen();
-}
-
-// async function lockPortrait() {
-//   await goFullScreen();
-//   await screen.orientation.lock("portrait");
+// function goFullScreen() {
+//   if (document.fullscreenElement) return;
+//   return document.documentElement.requestFullscreen();
 // }
 
-// async function lockLandscape() {
-//   await goFullScreen();
-//   await screen.orientation.lock("landscape");
+// // async function lockPortrait() {
+// //   await goFullScreen();
+// //   await screen.orientation.lock("portrait");
+// // }
+
+// // async function lockLandscape() {
+// //   await goFullScreen();
+// //   await screen.orientation.lock("landscape");
+// // }
+
+// function show(event) {
+//   const { type, angle } = screen.orientation;
+//   console.log(`Orientation type is ${type} + angle is ${angle}.`);
+//   console.log("Event Type is " + event.type);
 // }
 
-function show(event) {
-  const { type, angle } = screen.orientation;
-  console.log(`Orientation type is ${type} + angle is ${angle}.`);
-  console.log("Event Type is " + event.type);
-}
+// function oppOrientation() {
+//   const { type } = screen.orientation;
+//   if (type.startsWith("portrait")) {
+//     return "landscape";
+//   }
+//   return "portrait";
+// }
+// const rotateButton = document.getElementById("rotateButton");
 
-function oppOrientation() {
-  const { type } = screen.orientation;
-  if (type.startsWith("portrait")) {
-    return "landscape";
-  }
-  return "portrait";
-}
-const rotateButton = document.getElementById("rotateButton");
+// function btnOrientation() {
+//   const btnOrientation = oppOrientation();
+//   rotateButton.textContent = `Lock to ${btnOrientation}`;
+// }
 
-function btnOrientation() {
-  const btnOrientation = oppOrientation();
-  rotateButton.textContent = `Rotate to ${btnOrientation}`;
-}
+// async function rotate() {
+//   try {
+//     await goFullScreen();
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   const newOrientation = oppOrientation();
+//   await screen.orientation.lock(newOrientation);
+//   btnOrientation();
+// }
 
-async function rotate() {
-  try {
-    await goFullScreen();
-  } catch (err) {
-    console.error(err);
-  }
-  const newOrientation = oppOrientation();
-  await screen.orientation.lock(newOrientation);
-  btnOrientation();
-}
+// btnOrientation();
+// screen.orientation.addEventListener("change", btnOrientation);
+// screen.orientation.addEventListener("change", show);
+// window.addEventListener("load", show);
 
-btnOrientation();
-screen.orientation.addEventListener("change", btnOrientation);
-screen.orientation.addEventListener("change", show);
-window.addEventListener("load", show);
-
-function ready() {
-  console.log(
-    "Now full screen and ready to start, locked to " + screen.orientation.type
-  );
-}
+// function ready() {
+//   console.log(
+//     "Now full screen and ready to start, locked to " + screen.orientation.type
+//   );
+// }
 
 // async function start() {
 //   const promiseToFullScreen = new Promise(resolve =>
@@ -91,3 +91,44 @@ function ready() {
 //   });
 //   alert("To start, please rotate your screen to landscape.");
 // }
+
+function fullScreenCheck() {
+  if (document.fullscreenElement) return;
+  return document.documentElement.requestFullscreen();
+}
+
+const lockButton = document.getElementById("lockBtn");
+
+function btnOrientation() {
+  const btnOrientation = oppOrientation();
+  lockButton.textContent = `Lock to ${btnOrientation}`;
+}
+
+function oppOrientation() {
+  const { type } = screen.orientation;
+  if (type.startsWith("portrait")) {
+    return "landscape";
+  }
+  return "portrait";
+}
+
+async function rotate() {
+  const rotateBtn = document.getElementById("rotateBtn");
+  try {
+    await fullScreenCheck();
+  } catch (err) {
+    console.error(err);
+  }
+  const newOrientation = oppOrientation();
+  await screen.orientation.lock(newOrientation);
+  btnOrientation();
+}
+
+function show() {
+  const { type, angle } = screen.orientation;
+  console.log(`Orientation type is ${type} & angle is ${angle}.`);
+}
+
+screen.orientation.addEventListener("change", show);
+screen.orientation.addEventListener("change", btnOrientation);
+window.addEventListener("load", show);
